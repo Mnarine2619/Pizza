520c7076740012b3876c45df5ca5538c7af7a892
Mnarine2619 <mnarine2619@richmondhillhs.org>
2020-12-03 21:29:32 +0000

    Finalpizza

--- src/components/bbq.js
+++ src/components/bbq.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import Bbq from './images/bbq.jpg';
+import './css/slice.css';
 
-function Bbq() {
+function bbq() {
   return(
-    <div>
-
+  <div className="Bbq col-4">
+      <div className="card">
+        <img src={ bbq} class="card-img-top" alt="..."/>
+        <div className="card-body">
+          <h5 className="card-title">Bbq Slice</h5>
+          <p className="card-text">Try the most crispy and delicious bbq slice for just a dollar.</p>
+        </div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">Price: $1.00</li>
+        </ul>
+      </div>
     </div>
   )
 }
